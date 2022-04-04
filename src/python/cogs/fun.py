af4e9fc1534485195da7e5e508255a8e58df9b74
WoozyDragon4018 <58138055+WoozyDragon4018@users.noreply.github.com>
2022-04-04 22:08:21 +0530

    Create cogs dir and fun cog.

---
+++ src/python/cogs/fun.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+class Fun(commands.Cog, name="Fun Commands"):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @commands.command()
+    async def test(self, ctx):
+        await ctx.send("test pass")
+        
+def setup(bot):
+    bot.add_cog(Fun(bot))

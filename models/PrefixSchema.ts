acc9ffdd997a8901b2045c9eae3ecb19880ad42f
loldonut <loldonutph@gmail.com>
2022-06-19 21:44:05 +0800

    Add models folder and `PrefixSchema` model

---
+++ models/PrefixSchema.ts
@@ -0,0 +1,17 @@
+import { Schema, model, models } from 'mongoose';
+
+const reqString = {
+    type: String,
+    required: true
+};
+
+const PrefixSchema = new Schema({
+    _id: reqString,
+    prefix: reqString,
+    dateChanged: {
+        type: Date,
+        required: false
+    }
+});
+
+export default models['prefix-schema'] || model('prefix-schema', PrefixSchema);

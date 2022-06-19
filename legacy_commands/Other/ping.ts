129746420063ebfea057c3d07bd23dde5cf8b2f0
loldonut <loldonutph@gmail.com>
2022-06-19 21:01:52 +0800

    Port `ping` legacy command to TS

---
+++ legacy_commands/Other/ping.ts
@@ -0,0 +1,15 @@
+import type { CommandOptions } from '../../typings/index';
+
+export default {
+    name: 'ping',
+    description: 'Shows the ping of the Bot',
+    author: 'loldonut',
+
+    category: 'Other',
+
+    async execute(message) {
+        await message.channel.send({
+            content: `Pong! | ${message.client.ws.ping}ms`,
+        });
+    }
+} as CommandOptions;

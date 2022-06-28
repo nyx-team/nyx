cedbdde6f1a4fb7dab31ab3c8d6d8b2cb804bf72
loldonut <loldonutph@gmail.com>
2022-06-28 19:18:54 +0800

    Add `snipe` legacy command

---
+++ src/bot/events/messageDelete.ts
@@ -0,0 +1,16 @@
+import { EventOptions } from '../../typings';
+
+export default {
+    name: 'messageDelete',
+
+    async execute(client, message) {
+        if (message.author.bot) return;
+        client.snipedMessages.set(message.channel.id, message);
+
+        // Delete the sniped messages
+        // after 5 seconds
+        setTimeout(() => {
+            client.snipedMessages.delete(message.channel.id);
+        }, 1000 * 5);
+    }
+} as EventOptions;
---
+++ src/bot/legacy_commands/Fun/snipe.ts
@@ -0,0 +1,37 @@
+import { Message, MessageEmbed } from 'discord.js';
+import { CommandOptions } from '../../../typings';
+
+export default {
+    name: 'snipe',
+    description: 'Snipes a message that has been deleted on the target channel',
+
+    reqPerms: ['MANAGE_MESSAGES'],
+
+    category: 'Fun',
+
+    async execute(message: Message) {
+        const msg = message.client.snipedMessages.get(message.channel.id);
+
+        if (!msg) {
+            message.reply({
+                content: ':x: No message to snipe!',
+            });
+            return;
+        }
+
+        const embed = new MessageEmbed()
+            .setAuthor({
+                name: `${msg.author.tag}`,
+                iconURL: msg.member
+                    .displayAvatarURL({ dynamic: true }),
+            })
+            .setDescription(`>>> ${msg.content}`)
+            .setColor(`${msg.member.displayHexColor}`)
+            .setTimestamp()
+            .setImage(msg.attachments.first()?.proxyURL ?? null);
+
+        await message.reply({
+            embeds: [embed],
+        });
+    }
+} as CommandOptions;

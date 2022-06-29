c1e5486b5edfafd5523b02bb9ff390ef1f51925c
loldonut <loldonutph@gmail.com>
2022-06-29 17:07:26 +0800

    Add `purge` slash command

---
+++ src/bot/commands/purge.ts
@@ -0,0 +1,31 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandOptions } from '../../typings';
+
+export default {
+    name: 'purge',
+    data: new SlashCommandBuilder()
+        .setName('purge')
+        .setDescription('Purges messages / Bulk deletes messages')
+        .addNumberOption((option) =>
+            option
+                .setName('amount')
+                .setDescription('Amount of messages you want to be deleted')
+                .setRequired(true)
+        ),
+
+    async execute(interaction, options) {
+        const amount = options.getNumber('amount');
+
+        const { size } = await interaction.channel.bulkDelete(amount, true);
+
+        await interaction.reply({
+            content: `**Purged ${size} messages(s)!**`,
+        });
+
+        /* eslint-disable no-promise-executor-return */
+        const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+        await sleep();
+
+        await interaction.deleteReply();
+    }
+} as SlashCommandOptions;

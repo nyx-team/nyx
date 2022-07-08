d7b5253b09329920daf6c4f0371166d67c2c690f
loldonut <loldonutph@gmail.com>
2022-07-08 18:59:31 +0800

    Add `dice` slash command

---
+++ src/bot/commands/dice.ts
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandOptions } from '../../typings';
+
+import { randomInt } from 'crypto';
+
+export default {
+    name: 'dice',
+    data: new SlashCommandBuilder()
+        .setName('dice')
+        .setDescription('Roll a dice!'),
+    
+    async execute(interaction) {
+        const rolled = randomInt(1, 6);
+
+        await interaction.reply({
+            content: `**Rolled \`${rolled}\`!**`
+        });
+    },
+} as SlashCommandOptions;

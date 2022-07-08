edb8be895af227b9c697dd845234d8b6209b681a
loldonut <loldonutph@gmail.com>
2022-07-08 18:43:15 +0800

    Add `flip` slash command

---
+++ src/bot/commands/flip.ts
@@ -0,0 +1,19 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { SlashCommandOptions } from '../../typings';
+
+export default {
+    name: 'flip',
+    data: new SlashCommandBuilder()
+        .setName('flip')
+        .setDescription('Flip a coin'),
+    
+    async execute(interaction) {
+        const coin = ['Heads', 'Tails'];
+        const n = Math.floor(Math.random() * coin.length);
+        const flipped = coin[n];
+
+        await interaction.reply({
+            content: `Results: **${flipped}**`
+        });
+    },
+} as SlashCommandOptions;

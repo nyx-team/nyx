574b3342b1cec8d5924e74f29c0f8464fb4c4b52
loldonut <loldonutph@gmail.com>
2022-07-26 21:21:59 +0800

    feat: listen to `guildDelete` events to delete configs about the server

---
+++ src/bot/events/guildDelete.ts
@@ -0,0 +1,11 @@
+import { EventOptions } from '../../typings';
+import PrefixSchema from '../models/PrefixSchema';
+
+export default {
+    name: 'guildDelete',
+
+    async execute(_, guild) {
+        if (!guild.available) return;
+        await PrefixSchema.findByIdAndDelete(guild.id);
+    },
+} as EventOptions<'guildDelete'>;

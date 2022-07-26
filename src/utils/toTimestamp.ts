073e123cc1b96adccfecf21bd0a604699f422636
loldonut <loldonutph@gmail.com>
2022-07-26 15:37:02 +0800

    refactor(totimestamp): use discord.js `Formatters` instead

--- src/utils/toTimestamp.ts
+++ src/utils/toTimestamp.ts
@@ -1,7 +1,9 @@
+import { Formatters, TimestampStylesString } from 'discord.js';
+
 /**
  * Converts a JS Date number to Discord Timestamp Compatible
  * @param {number} date
  */
-export default function toTimestamp(date: number): number {
-    return Math.floor(date / 1000);
+export default function toTimestamp(date: number, type?: TimestampStylesString): string {
+    return Formatters.time(new Date(date), type);
 }
